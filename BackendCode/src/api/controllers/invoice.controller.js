808cb674ae7df39fd7534d4294e1be359164b6dd
Ashwin Balaji <671976@cognizant.com>
2018-12-31 18:47:10 +0530

    till mongo find all records

--- BackendCode/src/api/controllers/invoice.controller.js
+++ BackendCode/src/api/controllers/invoice.controller.js
@@ -1,24 +1,42 @@
-import Invoice from "../models/invoice.models";
+import Invoice from '../models/invoice.models';
+import Joi from 'joi';
 export default {
   findAll(req, res, next) {
-    console.log("inside findAll");
-    Invoice.find().then(invoices => res.json(invoices));
+    console.log('inside findAll');
+    Invoice.find()
+      .then(invoices => res.json(invoices))
+      .catch(err => {
+        return res.status(400).json(error);
+      });
   },
   create(req, res, next) {
-    console.log("inside create", req.body);
-    let error = new Error({ message: "error from create" });
-    error.status = 404;
-    next();
-    let { item, qty, date, due, tax, rate } = req.body;
-    if (!item) {
-      return res.status(400).json({ err: "item is required field" });
+    console.log('inside create', req.body);
+    // let error = new Error({ message: 'error from create' });
+    // error.status = 404;
+    // next(error);
+    // let { item, qty, date, due, tax, rate } = req.body;
+
+    const schema = Joi.object().keys({
+      item: Joi.string().required(),
+      date: Joi.date().required(),
+      due: Joi.date().required(),
+      tax: Joi.number().optional(),
+      rate: Joi.date().required(),
+      qty: Joi.number().required()
+    });
+
+    const { error, value } = Joi.validate(req.body, schema);
+    // console.log('value of req', value);
+    if (error && error.details) {
+      return res.status(400).json(error);
     }
-    Invoice.create({ item, qty, date, due, tax, rate })
+
+    Invoice.create(value)
       .then(invoice => {
         res.json(invoice);
       })
       .catch(err => {
-        res.status(500).json({ err: "error while inserting data" });
+        res.status(500).json({ err: 'error while inserting data' });
       });
   }
 };
--- BackendCode/src/app.js
+++ BackendCode/src/app.js
@@ -1,36 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import { router } from "./config/routes";
+import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './config/routes';
+import logger from 'morgan';
+
 let app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.Promise = global.Promise;
-app.use("/api", router);
-app.get("/", (req, res) => {
-  res.json({
-    mes: "welcome"
-  });
-});
 mongoose.connect(
-  "mongodb://localhost/invoice-builder",
+  'mongodb://localhost/invoice-builder',
   { useNewUrlParser: true }
 );
 
-// app.use((req, res, next) => {
-//   const error = new Error("Not found");
-//   error.message = "Invalid route";
-//   error.status = 404;
-//   next(error);
-// });
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.json({
-//     error: {
-//       err: err.message
-//     }
-//   });
-// });
+app.get('/', (req, res) => {
+  res.json({
+    mes: 'welcome'
+  });
+});
+
+app.use('/api', router);
+
 app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.message = "Invalid route";
+  const error = new Error('Not found');
+  error.message = 'Invalid route';
   error.status = 404;
   next(error);
 });
@@ -43,5 +37,5 @@ app.use((error, req, res, next) => {
   });
 });
 app.listen(3000, () => {
-  console.log("listening to port 3000");
+  console.log('listening to port 3000');
 });
