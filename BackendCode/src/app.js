02b9ffadff788964be0459b08290fdac42f5c4f3
ashwin <ashwin.achu.1311@gmail.com>
2019-01-01 20:06:04 +0530

    till post swagger

--- BackendCode/src/app.js
+++ BackendCode/src/app.js
@@ -1,30 +1,39 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { router } from './config/routes';
-import logger from 'morgan';
+import express from "express";
+import mongoose from "mongoose";
+import { router } from "./config/routes";
+import logger from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./config/swagger.json";
 
 let app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true
+  })
+);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  'mongodb://localhost/invoice-builder',
+  "mongodb://localhost/invoice-builder",
   { useNewUrlParser: true }
 );
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    mes: 'welcome'
+    mes: "welcome"
   });
 });
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.message = 'Invalid route';
+  const error = new Error("Not found");
+  error.message = "Invalid route";
   error.status = 404;
   next(error);
 });
@@ -37,5 +46,5 @@ app.use((error, req, res, next) => {
   });
 });
 app.listen(3000, () => {
-  console.log('listening to port 3000');
+  console.log("listening to port 3000");
 });
