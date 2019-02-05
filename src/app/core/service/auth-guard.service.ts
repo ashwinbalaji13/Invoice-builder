de28a38bf392d6219619a3b4d6ed61eaf687d39d
ashwin <ashwin.achu.1311@gmail.com>
2019-02-05 22:32:30 +0530

    fixed for social login,styling for button

--- src/app/core/service/auth-guard.service.ts
+++ src/app/core/service/auth-guard.service.ts
@@ -1,47 +1,50 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router, CanActivateChild, ActivatedRoute, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { JwtService } from '../jwt.service';
-import { AuthService } from './auth.service';
-import { Observable } from 'rxjs/Observable';
-import { map, catchError } from 'rxjs/operators';
-import { of as ObservableOf } from 'rxjs/observable/of';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  Router,
+  CanActivateChild,
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { JwtService } from "../jwt.service";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs/Observable";
+import { map, catchError } from "rxjs/operators";
+import { of as ObservableOf } from "rxjs";
 
 @Injectable()
 export class AuthGuardService implements CanActivate, CanActivateChild {
-
-  constructor(private jwtService: JwtService, private router: Router, private authService: AuthService) { }
-  canActivate(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
-
+  constructor(private jwtService: JwtService, private router: Router, private authService: AuthService) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     if (this.jwtService.getToken()) {
       return ObservableOf(true);
     }
-    const token = route.queryParamMap.get('token');
+    const token = route.queryParamMap.get("token");
     if (token) {
       return this.authService.isAunthenticate(token).pipe(
         map(authenticated => {
           if (authenticated === true) {
             this.jwtService.setToken(token);
-            this.router.navigate(['/dashboard', 'invoices']);
+            this.router.navigate(["/dashboard", "invoices"]);
             return true;
           }
-          this.router.navigate(['/login']);
+          this.router.navigate(["/login"]);
           return false;
         }),
         catchError((err: any) => {
-          this.router.navigate(['/login']);
+          this.router.navigate(["/login"]);
           return ObservableOf(false);
         })
       );
     }
     //authenticati
     else {
-      this.router.navigate(['/login']);
-      return Observable.of(false);
+      this.router.navigate(["/login"]);
+      return ObservableOf(false);
     }
   }
-  canActivateChild(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.canActivate(route, state);
   }
 }
